8bd507439b1ccd589820321badabd22263864ab5
ivanchatenko <seregha666@gmail.com>
2019-05-15 20:30:33 +0300

    Update_menu

--- BookProgress/Program.cs
+++ BookProgress/Program.cs
@@ -5,9 +5,15 @@ namespace BookProgress
 {
     public class Book
     {
+        // default constructor
+        public Book()
+        {
+            pages = chapters = 0;
+        }
         uint pages;
-        public uint myPages { get { return pages; } set { pages = (uint)(value > 0 ? value : -value); } }
-        public uint chapters;
+        public uint myPages { get { return pages; } set { pages = value; } }
+        uint chapters;
+        public uint myChapters { get { return chapters; } set { chapters = value; } }
         public uint current_page { get;  set; }
         public uint current_chapter { get; set; }
         public Book(uint pages,uint chapters)
@@ -67,7 +73,7 @@ namespace BookProgress
                 if (book.current_page > (book.pages / 2)) return true;
                 else return false;
             }
-            else return false;
+                else return false;
         }
         // reference example
         public static void LetRef(ref int a)
@@ -77,11 +83,12 @@ namespace BookProgress
         static void Main(string[] args)
         {
             Book Shieldt = new Book(1465, 20);
-            PagesLeft(Shieldt);
-            //ChaptersLeft(Shieldt);
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1 - Enter new book");
+            string input = Console.ReadLine();
+            if (input == "1") { 
+                Book book1 = new Book(1, 1); }
             //Console.WriteLine("Is half passed? Answer : " + IsHalfPassed(Shieldt)); 
-
-
             Console.ReadKey();
         }
     }
